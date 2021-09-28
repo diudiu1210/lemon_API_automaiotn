9deb6810a28fde0224c0a915b7645855bfcf1ed0
diudiu1210 <584819505@qq.com>
2021-09-28 21:11:50 +0800

    full file upload

---
+++ src/test/java/com/lemon/env/Constants.java
@@ -0,0 +1,22 @@
+package com.lemon.env;
+
+/**
+ * @author Automation engineer
+ * @Description The God of automation
+ * @Date 2021/9/18 17:56
+ */
+public class Constants {
+
+    public static final String BASH_URL = "http://api.lemonban.com/futureloan";
+
+    //配置debug环境开关
+    public static final boolean IS_DEBUGLOG =true;
+
+    //测试用例配置
+    public static final String V2_CASE = "src/test/resources/futureloan_API_Test cases_v2.xlsx";
+    public static final String V3_CASE ="src/test/resources/futureloan_API_Test cases_v3.xlsx";
+
+    //配置V3接口开关
+    public static final boolean IS_V3 =true;
+
+}
